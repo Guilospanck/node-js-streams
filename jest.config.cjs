b682973d9ec9b69c96cf33708aa6be42ab7901b0
Guilherme Rodrigues <guilherme.rp2010@hotmail.com>
2022-03-12 20:08:57 -0300

    test: server handler

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,4 +2,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  forceExit: true, // https://stackoverflow.com/questions/68437734/jest-has-detected-the-following-1-open-handle-potentially-keeping-jest-from-exit
+  silent: true // https://stackoverflow.com/questions/44467657/jest-better-way-to-disable-console-inside-unit-tests
 };
--- src/server.spec.ts
+++ src/server.spec.ts
@@ -1,4 +1,29 @@
-import { run, server } from './server';
+import { IncomingMessage } from 'http';
+import { Socket } from 'net';
+import { StreamData } from './client';
+import { Server } from './server';
+
+jest.mock('http', () => ({
+  createServer: jest.fn((handler: any) => ({
+    listen: jest.fn(() => ({
+      on: jest.fn(() => ({}))
+    })),
+  })),
+  IncomingMessage: jest.fn(() => ({
+    method: "GET"
+  }))
+}))
+
+const { ServerResponse } = jest.requireActual('http');
+
+
+function makeSut() {
+  const server = new Server();
+
+  return {
+    server
+  };
+}
 
 describe("Server", () => {
 
@@ -6,8 +31,38 @@ describe("Server", () => {
     jest.clearAllMocks();
   });
 
-  it("Expect true to be true", () => {
-    expect(true).toBe(true);
+  describe("generator function 'run'", () => {
+    test("it should generate data successfully", () => {
+      // arrange
+      const sut = makeSut();
+
+      // act
+      const result = sut.server.run();
+      const item1: StreamData = result.next().value as StreamData;
+      const item2: StreamData = result.next().value as StreamData;
+      const item3: StreamData = result.next().value as StreamData;
+
+      // assert
+      expect(item1.name).toEqual("Guilherme-0");
+      expect(item2.name).toEqual("Guilherme-1");
+      expect(item3.name).toEqual("Guilherme-2");
+
+    });
+  });
+
+  describe("handler", () => {
+    test("", async () => {
+      // arrange
+      const sut = makeSut();
+      const writableStreamMocked = new ServerResponse(IncomingMessage);      
+
+      // act
+      const result = await sut.server.handler(new IncomingMessage(new Socket), writableStreamMocked);
+
+      // assert
+      expect(result).resolves;
+
+    });
   });
 
 });
